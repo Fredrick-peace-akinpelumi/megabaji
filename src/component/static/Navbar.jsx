440cd7a8409ce9daedf6093d8c0f94d2609e9666
Aremu Olakunle <107206801+COL-js@users.noreply.github.com>
2023-02-28 11:50:35 +0100

    added new navlink to navbar.jsx

--- src/component/static/Navbar.jsx
+++ src/component/static/Navbar.jsx
@@ -10,69 +10,76 @@ import logo from '../images/logo.PNG'
 
 const Navbar = () => {
   const [show, setShow] = React.useState(false);
+  const isLoggedIn=true;
   return (
     <>
-  <nav className="navbar bg-body-tertiary fixed-top" style={{backgroundColor:"rgb(0,93,171)"}}>
-  <div className="container">
-    <img className="navbar-brand logo" src={logo} alt="" />
-    <div className="d-flex justify-space-between" >
-      <button className="btn text-white" data-bs-toggle="modal" data-bs-target="#exampleModal" style={{backgroundColor:"rgb(35,134,228)"}} >Login</button>
-      <Link to='/signup'><button className="btn text-white ms-5"  style={{backgroundColor:"rgb(101,204,48)"}}>Sign Up</button></Link>
-      {/* <button></button> */}
-    </div>
-  </div>
-</nav>
-<div className='fixed-top topp' >
-
-<nav className="navbar navbar-expand-lg " style={{backgroundColor:"rgb(26,121,211)"}}>
-  <div className="container ">
-    <a className="navbar-brand text-white fs-4" href="/"><i className="fa-solid fa-house"></i></a>
-    <a className="navbar-brand text-white fs-4" href="/">
-      <i class="fa-sharp fa-solid fa-download"></i>
-      </a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" >
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav spre">
-        <li className="nav-item">
-          <a className="nav-link  text-white mouse" aria-current="page"  onMouseOver={()=>setShow(true)} onMouseOut={()=>setShow(false)}>Sport<i className="fa-solid fa-angle-down"></i></a>
-
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link text-white" to='/casino'>Casino</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link text-white" to='/slot'>Slot</Link>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link text-white" href="/">Table</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link text-white" href="/">Lottery</a>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link text-white" to='/promotion'>Promotion</Link>
-
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-      <div className="container-fluid hover" style={show?{backgroundColor:"rgb(42,98,173)",display:"block"}:{backgroundColor:"rgb(42,98,173)",display:"none"}}>
-        <div className="row">
-          <div className=" imag">
-            <img src={img1} alt="" />
-            <img src={img2} alt="" />
-            <img src={img3} alt="" />
-            <img src={img4} alt="" />
+      <nav className="navbar bg-body-tertiary fixed-top" style={{backgroundColor:"rgb(0,93,171)"}}>
+        <div className="container">
+          <img className="navbar-brand logo" src={logo} alt="" />
+          {/* IF USER IS LOGGED IN ,IT WILL SHOW THE FIRST DIV IF NOT, IT WILL SHOW THE DIV AFTER THE COLON */}
+          {isLoggedIn?
+            <div className="d-flex justify-space-between" >
+              <Link to='/login'><button className="btn text-white" style={{backgroundColor:"rgb(35,134,228)"}} >Deposit</button></Link>
+              <Link to='/signup'><button className="btn text-white ms-5"  style={{backgroundColor:"rgb(101,204,48)"}}>User</button></Link>
+              <Link to='/login'><button className="btn text-white ms-5" style={{backgroundColor:"rgb(0,0,0)"}} >Log Out</button></Link>
+            {/* <button></button> */}
+            </div>
+          :
+            <div className="d-flex justify-space-between" >
+              
+              <button className="btn text-white" data-bs-toggle="modal" data-bs-target="#exampleModal" style={{backgroundColor:"rgb(35,134,228)"}} >Login</button>
+              <Link to='/signup'><button className="btn text-white ms-5"  style={{backgroundColor:"rgb(101,204,48)"}}>Sign Up</button></Link>
+            
+            </div>
+          }
+        </div>
+      </nav>
+      <div className='fixed-top topp' >
+        <nav className="navbar navbar-expand-lg " style={{backgroundColor:"rgb(26,121,211)"}}>
+          <div className="container ">
+            <a className="navbar-brand text-white fs-4" href="/"><i className="fa-solid fa-house"></i></a>
+            <a className="navbar-brand text-white fs-4" href="/">
+              <i class="fa-sharp fa-solid fa-download"></i>
+            </a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav spre">
+                <li className="nav-item">
+                  <a className="nav-link  text-white mouse" aria-current="page"  onMouseOver={()=>setShow(true)} onMouseOut={()=>setShow(false)}>Sport<i className="fa-solid fa-angle-down"></i></a>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link text-white" to='/casino'>Casino</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link text-white" to='/slot'>Slot</Link>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link text-white" href="/">Table</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link text-white" href="/">Lottery</a>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link text-white" to='/promotion'>Promotion</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className="container-fluid hover" style={show?{backgroundColor:"rgb(42,98,173)",display:"block"}:{backgroundColor:"rgb(42,98,173)",display:"none"}}>
+          <div className="row">
+            <div className=" imag">
+              <img src={img1} alt="" />
+              <img src={img2} alt="" />
+              <img src={img3} alt="" />
+              <img src={img4} alt="" />
+            </div>
           </div>
         </div>
       </div>
-</div>
-
-<Login/>
-
+      <Login/>
     </>
   )
 }
